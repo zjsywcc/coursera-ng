9e6cea8bf11d72bb76c03382b0df7f1140a1cae6
zjsywcc <zjsywcc@163.com>
2016-11-07 00:17:28 +0800

    api url change

--- assignments/assignment5/assignment5-starter-code/src/common/common.module.js
+++ assignments/assignment5/assignment5-starter-code/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://zjsywcc.github.io/coursera-ng/assignments/assignment5/assignment5-starter-code')
+.constant('ApiPath', 'https://puchie-course5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
